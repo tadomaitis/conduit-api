eafe1d568c136eadc4cdf7d71ba8e66813a370a6
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-09-02 20:07:18 -0300

    test: ensure SignUpController returns 400 if no username is provided

---
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  it('Should return 400 if no username is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param error: username'))
+  })
+})
