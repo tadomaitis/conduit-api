3a95161c7b7d52e2a3c1924e1bd7de95cd735071
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-09-03 09:33:05 -0300

    refactor: ensure SignUpController.handle implements correct interfaces

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,7 +1,8 @@
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { HttpResponse, HttpRequest } from '@/presentation/protocols/http'
 
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['username', 'email', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
