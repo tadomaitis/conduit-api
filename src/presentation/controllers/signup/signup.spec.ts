847bc57442d7549790330843bf0569cb6c2c6d7d
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-09-03 22:08:54 -0300

    test: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -91,4 +91,21 @@ describe('SignUp Controller', () => {
     sut.handle(httpRequest)
     expect(isValid).toHaveBeenCalledWith('valid_email@mail.com')
   })
+
+  it('Should return 500 if EmailValidator throws', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = {
+      body: {
+        username: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new Error('Internal Server Error'))
+  })
 })
